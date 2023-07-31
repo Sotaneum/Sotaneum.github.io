10acacd1317e424b3fe6fef2ce9431ea2c73b9d3
Sotaneum <gnyontu39@gmail.com>
2023-07-31 22:08:15 +0900

    post layout 정리

--- app/posts/[post]/layout.tsx
+++ app/posts/[post]/layout.tsx
@@ -1,15 +1,27 @@
-import { DefaultProps } from "@/app/types";
+import { DynamicProps } from "@/app/types";
 import Keywords from "@/components/Keywords";
 import Comments from "@/components/Comments";
 import Title from "@/components/Title";
+import { toPost } from "@/lib/data";
+import DateString from "@/components/DateString";
 
-export default function PostLayout({ children }: DefaultProps) {
+export default function PostLayout({
+  params,
+  children,
+}: DynamicProps<{ post: string }>) {
+  const { title, groupTags, tags, date, coverImage } = toPost(params.post);
+
   return (
     <div>
-      <Title />
-      {children}
-      <Keywords />
-      <Comments />
+      <Title title={title} coverImage={coverImage} />
+      <div className="max-w-2xl mx-auto">
+        <div className="mb-6 text-lg">
+          <DateString date={date} />
+        </div>
+        {children}
+        <Keywords />
+        <Comments />
+      </div>
     </div>
   );
 }
