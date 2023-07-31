effc55cc6d3512211102cf87eb98bde50c0ee62c
Sotaneum <gnyontu39@gmail.com>
2023-07-31 22:07:46 +0900

    Title 추가

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,3 +1,29 @@
-export default function Title() {
-  return <div>타이틀</div>;
+import Image from "next/image";
+
+interface TitleProps {
+  title: string;
+  coverImage?: string;
+}
+
+export default function Title({ coverImage, title }: TitleProps) {
+  return (
+    <>
+      <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+        {title}
+      </h1>
+      {coverImage && (
+        <div className="mb-8 md:mb-8 sm:mx-0">
+          <div className="sm:mx-0">
+            <Image
+              src={coverImage}
+              alt={`Cover Image for ${title}`}
+              className="shadow-sm w-full"
+              width={1300}
+              height={630}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
