b70866c93ef7a9107fa5fdf8ca068f0876b01b9d
Sotaneum <gnyontu39@gmail.com>
2023-07-31 16:31:58 +0900

    댓글 추가

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import Giscus from "@giscus/react";
+
 export default function Comments() {
-  return <div>comments</div>;
+  return (
+    <Giscus
+      id="comments"
+      repo="Sotaneum/sotaneum.github.io"
+      repoId="MDEwOlJlcG9zaXRvcnkxOTcwMjMxODE="
+      category="Announcements"
+      categoryId="DIC_kwDOC75Vzc4CYOm9"
+      mapping="pathname"
+      term="test"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="preferred_color_scheme"
+      lang="ko"
+      loading="lazy"
+    />
+  );
 }
