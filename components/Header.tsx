0587797116d0993735b987936c8d59ae283d8d4b
Sotaneum <gnyontu39@gmail.com>
2023-07-31 16:32:10 +0900

    헤더 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <div>
-      <div>logo</div>
-      <div>menu</div>
+    <div className="md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+      <a href="/" className="inline-flex items-center">
+        <span className="text-4xl pl-2 select-none">Blog</span>
+        <Image
+          className="w-16 h-16 select-none"
+          src={"/favicon.ico"}
+          alt={"icon"}
+          width={18}
+          height={18}
+        />
+      </a>
+      <span className="text-sm whitespace-nowrap">
+        이것저것 해보는 블로그입니다.
+      </span>
     </div>
   );
 }
