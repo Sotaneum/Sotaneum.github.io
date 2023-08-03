b0b5d65b94a56cbface8ba417c0c16d8878667c0
Sotaneum <gnyontu39@gmail.com>
2023-08-03 23:26:37 +0900

    푸터 표시를 위한 기본 높이 지정

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,5 +1,5 @@
 import { DefaultProps } from "@/app/types";
 
 export default function ListLayout({ children }: DefaultProps) {
-  return <main>{children}</main>;
+  return <main style={{ minHeight: "calc(100vh - 12rem)" }}>{children}</main>;
 }
