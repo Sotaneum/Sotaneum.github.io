b0ac410fad50ec6a31754f1aa27f051d00455be5
Sotaneum <gnyontu39@gmail.com>
2023-08-03 23:26:48 +0900

    푸터 가운데 정렬 처리

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div>
+    <div className="flex justify-center items-center">
       <div>Copyright 2023 소금탄음료수(LEE DONG GUN)</div>
     </div>
   );
