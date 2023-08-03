b184e379541d1c1c209f74eed5fd5db6645f0a6a
Sotaneum <gnyontu39@gmail.com>
2023-08-03 23:44:08 +0900

    제목 크기 조절

--- components/Title.tsx
+++ components/Title.tsx
@@ -8,7 +8,7 @@ interface TitleProps {
 export default function Title({ coverImage, title }: TitleProps) {
   return (
     <>
-      <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+      <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
         {title}
       </h1>
       {coverImage && (
