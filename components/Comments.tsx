5aeb4a4eddbbe5b7672b3ba094180130528fd74a
Sotaneum <gnyontu39@gmail.com>
2023-08-03 23:43:13 +0900

    댓글 기본 높이 지정

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,20 +4,22 @@ import Giscus from "@giscus/react";
 
 export default function Comments() {
   return (
-    <Giscus
-      id="comments"
-      repo="Sotaneum/sotaneum.github.io"
-      repoId="MDEwOlJlcG9zaXRvcnkxOTcwMjMxODE="
-      category="Announcements"
-      categoryId="DIC_kwDOC75Vzc4CYOm9"
-      mapping="pathname"
-      term="test"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme="preferred_color_scheme"
-      lang="ko"
-      loading="lazy"
-    />
+    <div style={{ minHeight: "400px" }}>
+      <Giscus
+        id="comments"
+        repo="Sotaneum/sotaneum.github.io"
+        repoId="MDEwOlJlcG9zaXRvcnkxOTcwMjMxODE="
+        category="Announcements"
+        categoryId="DIC_kwDOC75Vzc4CYOm9"
+        mapping="pathname"
+        term="test"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="preferred_color_scheme"
+        lang="ko"
+        loading="lazy"
+      />
+    </div>
   );
 }
